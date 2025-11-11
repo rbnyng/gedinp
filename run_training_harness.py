1250fdaf6242ab88c3b1d2f28f15e8a49f15f275
Robin Young <59581322+rbnyng@users.noreply.github.com>
2025-11-11 12:26:25 +0000

    Update run_training_harness.py

--- run_training_harness.py
+++ run_training_harness.py
@@ -84,8 +84,8 @@ def parse_args():
 
     # Baseline specific arguments (for train_baselines.py)
     parser.add_argument('--models', type=str, nargs='+',
-                        default=['rf', 'xgb', 'idw'],
-                        choices=['rf', 'xgb', 'idw'],
+                        default=['rf', 'xgb', 'idw', 'lr'],
+                        choices=['rf', 'xgb', 'idw', 'lr'],
                         help='[train_baselines.py only] Which baseline models to train')
 
     args = parser.parse_args()
